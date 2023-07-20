36792e43262e551ee279a11266d85c9f8380b9fa
Joseph Corrado <josephcorrado7@gmail.com>
2023-07-20 16:25:59 -0400

    refactor GCP cloud vision code our of controller

--- 3_computer_vision/ml_project_4_face_detection/backend/app.py
+++ 3_computer_vision/ml_project_4_face_detection/backend/app.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+from base64 import b64encode, b64decode
+from service_layer import detect_faces
 from google.cloud import vision
-from base64 import b64encode
-from typing import List
-from loguru import logger
 
 app = FastAPI()
+client = vision.ImageAnnotatorClient()
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,28 +21,13 @@ async def hello_world():
     return {"message": "Hello World"}
 
 @app.post("/detect_faces")
-async def detect_faces(file: UploadFile):
+async def post_detect_faces(file: UploadFile):
     """
     An endpoint to send uploaded images to GCP to detect faces on an image
     """
-    # client = vision.ImageAnnotatorClient()
-
-    # Read the image file
     contents = await file.read()
-    
-    # Encode the image file to base64
     img_base64 = b64encode(contents)
+    output_image_bytes = await detect_faces(img_base64, client)
+    return b64decode(output_image_bytes.getvalue()).decode("utf-8")
 
-    decoded_image = img_base64.decode('utf-8')
 
-    return {"image": decoded_image}
-    
-    # # Create an image object from the base64 string
-    # image = vision.Image(content=img_base64)
-    
-    # # Perform face detection on the image
-    # response = client.face_detection(image=image)
-    # face_annotations = response.face_annotations
-
-    # # Return the face annotations
-    # return face_annotations
