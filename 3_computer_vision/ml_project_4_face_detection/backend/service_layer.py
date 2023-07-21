314aac15dd96fb0cff685d175a0780693fb8b288
Joseph Corrado <josephcorrado7@gmail.com>
2023-07-20 21:56:00 -0400

    add face detection bounding box drawing

--- 3_computer_vision/ml_project_4_face_detection/backend/service_layer.py
+++ 3_computer_vision/ml_project_4_face_detection/backend/service_layer.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from image_utils import (
     draw_boxes, 
     get_gcp_bounding_boxes,
@@ -5,17 +6,19 @@ from image_utils import (
 )
 import io
 
-async def detect_faces(img_base64, client) -> io.BytesIO:
-    detected_faces = get_gcp_bounding_boxes(img_base64, client)
+async def detect_faces(contents: io.BytesIO, client) -> io.BytesIO:
+    contents = Image.open(contents)
+    if contents.mode != 'RGB':
+        contents = contents.convert('RGB')
+
+    byte_arr = io.BytesIO()
+    contents.save(byte_arr, format='PNG')
+    byte_data = byte_arr.getvalue()
+    detected_faces = get_gcp_bounding_boxes(byte_data, client)
 
     print_face_diagnostics(detected_faces)
 
-    image_bytes = draw_boxes(uri, face_annotations)
-
-    if response.error.message:
-        raise Exception(
-            "{}\nFor more info on error messages, check: "
-            "https://cloud.google.com/apis/design/errors".format(response.error.message)
-        )
-
-    return image_bytes
+    output_image = draw_boxes(byte_arr, detected_faces)
+    output_image_stream = io.BytesIO()
+    output_image.save(output_image_stream, format="PNG")
+    return output_image_stream.getvalue()
