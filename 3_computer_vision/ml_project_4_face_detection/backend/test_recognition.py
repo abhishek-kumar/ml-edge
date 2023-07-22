789847bd292c842fbe9fadfb5126904d5f384046
Joseph Corrado <josephcorrado7@gmail.com>
2023-07-22 15:24:24 -0400

    add parametrized rekognition fixture

--- 3_computer_vision/ml_project_4_face_detection/backend/test_recognition.py
+++ 3_computer_vision/ml_project_4_face_detection/backend/test_recognition.py
@@ -3,21 +3,45 @@ from recognition import detect_celebrity
 import pathlib
 from botocore.session import Session
 from loguru import logger
+import pytest
 
-def test_detect_celebrity():
-    PATH_TO_TEST_IMAGE = pathlib.Path("Celebrity Faces Dataset/Brad Pitt/001_c04300ef.jpg")
-    # PATH_TO_TEST_IMAGE = pathlib.Path(".test_assets/test_image.jpg")
-    assert PATH_TO_TEST_IMAGE.exists(), f"Could not find {PATH_TO_TEST_IMAGE}"
+# @pytest.fixture
+# def test_image():
+#     # PATH_TO_TEST_IMAGE = pathlib.Path("Celebrity Faces Dataset/Brad Pitt/001_c04300ef.jpg")
+#     PATH_TO_TEST_IMAGE = pathlib.Path(".test_assets/test_image.jpg")
+#     assert PATH_TO_TEST_IMAGE.exists(), f"Could not find {PATH_TO_TEST_IMAGE}"
+    
+#     with open(PATH_TO_TEST_IMAGE, "rb") as image_file:
+#         content = image_file.read()
+
+#     return content
+
+
+@pytest.mark.parametrize(
+        "test_image_path,expected_output", 
+        [
+            (pathlib.Path("Celebrity Faces Dataset/Brad Pitt/001_c04300ef.jpg"), "Brad Pitt"), 
+            (pathlib.Path(".test_assets/test_image.jpg"), None)
+        ]
+)
+def test_rekognition_can_detect_brad_pitt(test_image_path, expected_output):
+    """
+    Test that AWS Rekognition can detect Brad Pitt, but not the example image.
+    """
+    assert test_image_path.exists(), f"Could not find {test_image_path}"
+    
+    with open(test_image_path, "rb") as image_file:
+        content = image_file.read()
 
     rekognition_session = Session(profile="rekognition")
     rekognition_client = rekognition_session.create_client(
         service_name="rekognition",
         region_name="us-east-1"
     )
-
-    with open(PATH_TO_TEST_IMAGE, "rb") as image_file:
-        content = image_file.read()
-    
     response = detect_celebrity(content, rekognition_client)
-
     logger.info(f"Detected celebrities:\n{response}")
+
+    assert response == expected_output
+
+def test_vertexai_fine_tuned_resnet_can_detect_brad_pitt():
+    pass
