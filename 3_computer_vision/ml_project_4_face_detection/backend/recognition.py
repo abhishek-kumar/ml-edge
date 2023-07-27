41fa86e80a9dc5d5d0f4e507aad3a1c232a58f76
Joseph Corrado <josephcorrado7@gmail.com>
2023-07-27 09:47:39 -0400

    call VertexAI to perform face recognition

--- 3_computer_vision/ml_project_4_face_detection/backend/recognition.py
+++ 3_computer_vision/ml_project_4_face_detection/backend/recognition.py
@@ -1,25 +1,37 @@
+import base64
+import io
+import json
+import pathlib
+from typing import Dict, List, Optional, Tuple
+
+import cv2
 import numpy as np
-from typing import Optional, Tuple
-from loguru import logger
-from google.cloud import aiplatform
+import PIL
 import tensorflow as tf
+from google.cloud import aiplatform
+from keras.applications.resnet50 import ResNet50, preprocess_input
+from loguru import logger
+from pydantic import BaseModel, validator
 from tensorflow.keras.preprocessing.image import (
-    load_img,
     DirectoryIterator,
     img_to_array,
+    load_img,
 )
-from keras.applications.resnet50 import ResNet50, preprocess_input
-import pathlib
-import PIL
-from typing import Dict
-import json
 
-DEFAULT_IMAGE_SIZE = (224, 224)
+DEFAULT_IMAGE_LEN = 112
+
+DEFAULT_IMAGE_SIZE = (DEFAULT_IMAGE_LEN, DEFAULT_IMAGE_LEN)
+PATH_TO_CELEBRITY_MAPPING = (
+    pathlib.Path(__file__).resolve().parent / "idx_to_celebrity_name.json"
+)
+assert PATH_TO_CELEBRITY_MAPPING.exists(), f"Could not find {PATH_TO_CELEBRITY_MAPPING}"
+
+with open(PATH_TO_CELEBRITY_MAPPING, "r") as f:
+    loaded_idx_to_celebrity_name = json.load(f)
+
 
 def detect_celebrity(image: bytes, rekognition_client) -> Optional[str]:
-    response = rekognition_client.recognize_celebrities(
-        Image={"Bytes": image}
-    )
+    response = rekognition_client.recognize_celebrities(Image={"Bytes": image})
     logger.info(f"Response:\n{response}")
 
     celebrities = response.get("CelebrityFaces")
@@ -27,26 +39,11 @@ def detect_celebrity(image: bytes, rekognition_client) -> Optional[str]:
     return response["CelebrityFaces"][0]["Name"] if len(celebrities) > 0 else None
 
 
-def vertex_ai_endpoint_predict_sample(
-    project: str, location: str, instances: list, endpoint_url: str
-):
-    """
-    Taken from https://cloud.google.com/vertex-ai/docs/predictions/get-online-predictions#predict-request
-    """
-    aiplatform.init(project=project, location=location)
-
-    endpoint = aiplatform.Endpoint(endpoint_url)
-
-    prediction = endpoint.predict(instances=instances)
-    logger.info(f"Input instances: {instances}")
-    logger.info(f"Prediction response: {prediction}")
-    return prediction
-
 def load_image_from_file(
-        image_path: pathlib.Path, 
-        target_image_size: Tuple[int, int]=DEFAULT_IMAGE_SIZE
+    image_path: pathlib.Path, target_image_size: Tuple[int, int] = DEFAULT_IMAGE_SIZE
 ) -> PIL.Image:
     return load_img(image_path, target_size=target_image_size)
+
 
 def load_local_model(model_path: pathlib.Path) -> Tuple[tf.keras.Model, Dict[int, str]]:
     """
@@ -54,12 +51,15 @@ def load_local_model(model_path: pathlib.Path) -> Tuple[tf.keras.Model, Dict[int
     mapping dictionary that has been saved as an asset
     """
     saved_model = tf.keras.models.load_model(model_path)
-    with open(saved_model.asset.asset_path.numpy(), 'r') as f:
-        loaded_idx_to_celebrity_name= json.load(f)
+    with open(saved_model.asset.asset_path.numpy(), "r") as f:
+        loaded_idx_to_celebrity_name = json.load(f)
 
     return saved_model, loaded_idx_to_celebrity_name
 
-def get_predicted_celebrity_name_from_idx(data_streamer: DirectoryIterator, predicted_idx: int) -> str:
+
+def get_predicted_celebrity_name_from_idx(
+    data_streamer: DirectoryIterator, predicted_idx: int
+) -> str:
     """
     The mapping from celebrity name to index in the data streamer needs to be
     inverted so that we can get the name of the predicted celebrity, given the
@@ -68,21 +68,98 @@ def get_predicted_celebrity_name_from_idx(data_streamer: DirectoryIterator, pred
     idx_to_celebrity_name = {v: k for k, v in data_streamer.class_indices.items()}
     return str(idx_to_celebrity_name.get(predicted_idx))
 
-def make_prediction(image: PIL.Image, model: tf.keras.Model, idx_to_celebrity_name: Dict[int, str]) -> Optional[str]:
-    example_image_array: np.ndarray  = img_to_array(image)
+
+def make_prediction(
+    image: PIL.Image, model: tf.keras.Model, idx_to_celebrity_name: Dict[int, str]
+) -> Optional[str]:
+    logger.info(f"Input image: {image}")
+    example_image_array: np.ndarray = img_to_array(image)
+    logger.info(f"Image array shape: {example_image_array.shape}")
     example_image_array = np.expand_dims(example_image_array, axis=0)
+    logger.info(f"After expanding dimensions: {example_image_array.shape}")
     example_image_array = preprocess_input(example_image_array)
+    logger.info(f"After preprocessing: {example_image_array.shape}")
 
     prediction_probabilities = model.predict(example_image_array)
 
     predicted_celebrity_index: int = int(np.argmax(prediction_probabilities))
     logger.info(f"Predicted celebrity index: {predicted_celebrity_index}")
 
-    return idx_to_celebrity_name.get(predicted_celebrity_index)
+    logger.info(f"Using idx_to_celebrity_name mapping: {idx_to_celebrity_name}")
+
+    celebrity_name_prediction: Optional[str] = idx_to_celebrity_name.get(
+        str(predicted_celebrity_index)
+    )
+
+    logger.info(f"Predicted celebrity name: {celebrity_name_prediction}")
+
+    return celebrity_name_prediction
 
 
+class Instance(BaseModel):
+    values: np.ndarray
+
+    @validator("values")
+    def check_shape(cls, values):
+        if values.shape != (*DEFAULT_IMAGE_SIZE, 3):
+            raise ValueError(f"Expected shape (224, 224, 3), got {values.shape}")
+        return values
+
+    class Config:
+        arbitrary_types_allowed = True
 
 
-def detect_celebrity_from_fine_tuned_resnet50(image: bytes, vertexai_client) -> Optional[str]:
+class RequestModel(BaseModel):
+    instances: List[Instance]
 
-    pass
+
+def vertex_ai_endpoint_predict_sample(
+    project: str, location: str, instances, endpoint_id: str = "2627296228710285312"
+) -> int:
+    """
+    Taken from https://cloud.google.com/vertex-ai/docs/predictions/get-online-predictions#predict-request
+
+    Live endpoints are used like this:
+    endpoint.predict(instances=[[6.7, 3.1, 4.7, 1.5], [4.6, 3.1, 1.5, 0.2]])
+    """
+    aiplatform.init(project=project, location=location)
+
+    # endpoint = aiplatform.Endpoint(endpoint_name=endpoint_id)
+    endpoint = aiplatform.Endpoint(
+        endpoint_name="projects/820240006900/locations/us-east4/endpoints/5089357849998393344"
+    )
+
+    logger.info(f"Input instances: {instances}")
+    prediction_probabilities = endpoint.predict(instances=instances).predictions
+    logger.info(f"Prediction response: {prediction_probabilities}")
+    predicted_celebrity_index: int = int(np.argmax(prediction_probabilities))
+    logger.info(f"Predicted celebrity index: {predicted_celebrity_index}")
+    return predicted_celebrity_index
+
+
+def detect_celebrity_from_fine_tuned_resnet50(image: bytes) -> Optional[str]:
+    # Convert bytes to image
+    image = PIL.Image.open(io.BytesIO(image))
+
+    # Preprocess the image
+    image = image.resize(
+        DEFAULT_IMAGE_SIZE
+    )  # replace with your model's expected dimensions
+    np_image = np.array(image)
+    np_image = np.expand_dims(np_image, axis=0)  # add a batch dimension
+
+    np_image = preprocess_input(np_image)
+
+    # Send it to the model
+    instances = np_image.tolist()  # convert the array to a list before sending
+
+    prediction = vertex_ai_endpoint_predict_sample(
+        project="aiedge-masterclass", location="us-east4", instances=instances
+    )
+
+    logger.info(f"Prediction response: {prediction}")
+
+    predicted_celebrity: Optional[str] = loaded_idx_to_celebrity_name.get(
+        str(prediction)
+    )
+    return predicted_celebrity
--- 3_computer_vision/ml_project_4_face_detection/backend/test_recognition.py
+++ 3_computer_vision/ml_project_4_face_detection/backend/test_recognition.py
@@ -1,20 +1,31 @@
 # from recognition import zero_shot, generate_image_to_id_mapping
-from recognition import (
-    detect_celebrity,
-    load_image_from_file,
-    make_prediction,
-    load_local_model,
-    DEFAULT_IMAGE_SIZE
-)
-from tensorflow.keras.preprocessing.image import (
-    img_to_array
-)
+import json
 import pathlib
-from botocore.session import Session
-from loguru import logger
-import pytest
-import PIL
+from typing import Optional
+
 import numpy as np
+import PIL
+import pytest
+from botocore.session import Session
+from google.cloud import aiplatform
+from keras.applications.resnet50 import preprocess_input
+from loguru import logger
+from tensorflow.keras.preprocessing.image import img_to_array
+
+from recognition import (
+    DEFAULT_IMAGE_SIZE,
+    detect_celebrity,
+    detect_celebrity_from_fine_tuned_resnet50,
+    load_image_from_file,
+    load_local_model,
+    make_prediction,
+)
+
+PATH_TO_CELEBRITY_MAPPING = pathlib.Path("idx_to_celebrity_name.json")
+assert PATH_TO_CELEBRITY_MAPPING.exists(), f"Could not find {PATH_TO_CELEBRITY_MAPPING}"
+
+with open(PATH_TO_CELEBRITY_MAPPING, "r") as f:
+    idx_to_celebrity_name = json.load(f)
 
 
 @pytest.fixture
@@ -25,55 +36,62 @@ def test_pil_image() -> PIL.Image:
     )
     return loaded_img_from_file
 
+
 @pytest.fixture
 def brad_pitt_image() -> PIL.Image:
-    brad_pitt_image_path = pathlib.Path("Celebrity Faces Dataset/Brad Pitt/001_c04300ef.jpg")
+    brad_pitt_image_path = pathlib.Path(
+        "Celebrity Faces Dataset/Brad Pitt/001_c04300ef.jpg"
+    )
     return load_image_from_file(brad_pitt_image_path)
 
 
 @pytest.mark.parametrize(
-        "test_image_path,expected_output", 
-        [
-            (pathlib.Path("Celebrity Faces Dataset/Brad Pitt/001_c04300ef.jpg"), "Brad Pitt"), 
-            (pathlib.Path(".test_assets/test_image.jpg"), None)
-        ]
+    "test_image_path,expected_output",
+    [
+        (
+            pathlib.Path("Celebrity Faces Dataset/Brad Pitt/001_c04300ef.jpg"),
+            "Brad Pitt",
+        ),
+        (pathlib.Path(".test_assets/test_image.jpg"), None),
+    ],
 )
 def test_rekognition_can_detect_brad_pitt(test_image_path, expected_output):
     """
     Test that AWS Rekognition can detect Brad Pitt, but not the example image.
     """
     assert test_image_path.exists(), f"Could not find {test_image_path}"
-    
+
     with open(test_image_path, "rb") as image_file:
         content = image_file.read()
 
     rekognition_session = Session(profile="rekognition")
     rekognition_client = rekognition_session.create_client(
-        service_name="rekognition",
-        region_name="us-east-1"
+        service_name="rekognition", region_name="us-east-1"
     )
     response = detect_celebrity(content, rekognition_client)
     logger.info(f"Detected celebrities:\n{response}")
 
     assert response == expected_output
 
+
 def test_vertexai_fine_tuned_resnet_can_detect_brad_pitt():
     pass
 
+
 def test_load_img_from_file(test_pil_image: PIL.Image):
     assert isinstance(test_pil_image, PIL.Image)
+
 
 def test_can_convert_pil_image_to_np_array(test_pil_image: PIL.Image):
     test_image_np_array: np.ndarray = img_to_array(test_pil_image)
     assert isinstance(test_image_np_array, np.ndarray)
     assert test_image_np_array.shape == (*DEFAULT_IMAGE_SIZE, 3)
 
+
 def test_can_make_local_prediction_on_fine_tuned_resnet(brad_pitt_image):
     # arrange
     path_to_model = pathlib.Path("../backend/model")
-    model = load_local_model(path_to_model)
-
-    idx_to_celebrity_name = {1: "Brad Pitt"}
+    model, idx_to_celebrity_name = load_local_model(path_to_model)
 
     # act
     prediction = make_prediction(brad_pitt_image, model, idx_to_celebrity_name)
@@ -82,6 +100,33 @@ def test_can_make_local_prediction_on_fine_tuned_resnet(brad_pitt_image):
     assert prediction == "Brad Pitt"
 
 
+def test_vertex_ai_request(brad_pitt_image):
+    example_image_array: np.ndarray = img_to_array(brad_pitt_image)
+    example_image_array = np.expand_dims(example_image_array, axis=0)
+    example_image_array = preprocess_input(example_image_array)
 
-def test_vertex_ai_request():
-    pass
+    aiplatform.init(
+        project="aiedge-masterclass",
+        location="us-east4",
+    )
+
+    endpoint = aiplatform.Endpoint("2627296228710285312")
+
+    logger.info(f"Example image array shape: {example_image_array.shape}")
+
+    prediction_probabilities = endpoint.predict([{"values": example_image_array}])
+
+    predicted_celebrity_index: int = int(np.argmax(prediction_probabilities))
+    logger.info(f"Predicted celebrity index: {predicted_celebrity_index}")
+
+    logger.info(f"Using idx_to_celebrity_name mapping: {idx_to_celebrity_name}")
+
+    celebrity_name_prediction: Optional[str] = idx_to_celebrity_name.get(
+        str(predicted_celebrity_index)
+    )
+
+    # act
+    prediction = detect_celebrity_from_fine_tuned_resnet50(brad_pitt_image)
+
+    # assert
+    assert prediction == "Brad Pitt"
