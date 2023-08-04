6f5355085ddd671fe28ca26d9326b5c628f576fc
Joseph Corrado <josephcorrado7@gmail.com>
2023-08-04 11:40:13 -0400

    add method to get reshaped t-SNE embeddings

--- 2_large_language_models/llm_masterclass/similar_words.py
+++ 2_large_language_models/llm_masterclass/similar_words.py
@@ -4,9 +4,13 @@ from typing import List
 # Third Party Imports
 import numpy as np
 from gensim.models.keyedvectors import KeyedVectors as GensimKeyedVectors
+from sklearn.manifold import TSNE
 
 
 def get_word_clusters(words, word2vec_model: GensimKeyedVectors, topn=30):
+    """
+    Given a list of words and a word2vec model, return a list of lists of similar words and a list of lists of similar word embeddings.
+    """
     word_clusters: List[List[str]] = []
     word_cluster_embeddings: List[List[np.ndarray]] = []
     # for each of our query words
@@ -23,3 +27,27 @@ def get_word_clusters(words, word2vec_model: GensimKeyedVectors, topn=30):
         word_clusters.append(most_similar_words)
 
     return word_clusters, np.array(word_cluster_embeddings)
+
+
+def create_tsne_model(
+    perplexity=15, n_components=2, init="pca", n_iter=3500, random_state=32, n_jobs=-1
+):
+    return TSNE(
+        perplexity=perplexity,
+        n_components=n_components,
+        init=init,
+        n_iter=n_iter,
+        random_state=random_state,
+        n_jobs=n_jobs,
+    )
+
+
+def get_reshaped_tsne_embeddings(word_cluster_embeddings_np, tsne_model):
+    """
+    Given a list of lists of word embeddings, return a list of lists of t-SNE embeddings.
+    """
+    # n samples, by m similar words, by d dimensions
+    n, m, d = word_cluster_embeddings_np.shape
+    tsne_training_set = word_cluster_embeddings_np.reshape(n * m, d)
+    t_sne_embeddings = tsne_model.fit_transform(tsne_training_set)
+    return t_sne_embeddings.reshape(n, m, 2)
