50810f4ca814755e97bb1e9ea61ebf0ddac5d447
Joseph Corrado <josephcorrado7@gmail.com>
2023-08-04 17:01:01 -0400

    finish dash plotly frontend for adding words

--- 2_large_language_models/ml_project_1_similar_words/app.py
+++ 2_large_language_models/ml_project_1_similar_words/app.py
@@ -1,3 +1,6 @@
+# Python Standard Library Imports
+import time
+
 # Third Party Imports
 import dash
 import matplotlib.cm as cm
@@ -6,12 +9,30 @@ import plotly.graph_objs as go
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 from gensim.models.keyedvectors import KeyedVectors as GensimKeyedVectors
+from loguru import logger
 
 # llm_masterclass Imports
 from llm_masterclass import google_news
 from llm_masterclass import similar_words as masterclass_similar_words
 
-words_to_start = ["Paris", "Python"]
+words_to_start = [
+    "Paris",
+    "Python",
+    "Sunday",
+    "Tolstoy",
+    "Twitter",
+    "bachelor",
+    "delivery",
+    "election",
+    "expensive",
+    "experience",
+    "financial",
+    "food",
+    "iOS",
+    "peace",
+    "release",
+    "war",
+]
 options = [{"label": word, "value": word} for word in words_to_start]
 
 word2vec_google_news_300: GensimKeyedVectors = google_news.get_google_news_model()
@@ -38,18 +59,37 @@ app.layout = html.Div(
 
 
 @app.callback(
-    Output("word-scatter", "figure"),
+    Output("dropdown-word", "value"),
     Output("dropdown-word", "options"),
     Input("button-add", "n_clicks"),
-    Input("button-delete", "n_clicks"),
     State("input-word", "value"),
-    State("dropdown-word", "value"),
     State("dropdown-word", "options"),
+    prevent_initial_call=True,
 )
-def update_graph(
-    n_clicks_add, n_clicks_delete, input_word, dropdown_word, dropdown_options
-):
-    words = [option["value"] for option in dropdown_options]
+def add_word(n_clicks, input_word, dropdown_options):
+    logger.info(f"Current dropdown options: {dropdown_options}")
+    current_words = [option["value"] for option in dropdown_options]
+    logger.info(f"Current words: {current_words}")
+    logger.info(f"Current word: {input_word}")
+    new_dropdown_options = [{"label": word, "value": word} for word in current_words]
+    if n_clicks == 0 and not input_word:
+        return new_dropdown_options
+    if input_word not in current_words:  # if the word is new, add it
+        new_dropdown_options.append({"label": input_word, "value": input_word})
+    logger.info(f"Setting dropdown options: {new_dropdown_options}")
+    new_words = [option["value"] for option in new_dropdown_options]
+    logger.info(f"Setting dropdown value to: {new_words}")
+    return new_words, new_dropdown_options
+
+
+@app.callback(
+    Output("word-scatter", "figure"),
+    Input("button-add", "n_clicks"),
+    Input("button-delete", "n_clicks"),
+    Input("dropdown-word", "value"),
+)
+def update_graph(n_clicks_add, n_clicks_delete, words):
+    logger.info(f"Plotting for words: {words}")
     # Get the colors in rgba format (values between 0 and 1)
     colors = cm.rainbow(np.linspace(0, 1, len(words)))
 
@@ -120,9 +160,7 @@ def update_graph(
         annotations=annotations,
     )
 
-    fig = go.Figure(data=data, layout=layout)
-    dropdown_options = [{"label": word, "value": word} for word in words]
-    return fig, dropdown_options
+    return go.Figure(data=data, layout=layout)
 
 
 if __name__ == "__main__":
